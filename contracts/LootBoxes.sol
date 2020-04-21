07b513061ea9ba168227f8b2531904beb98f69a2
Michael Bucher <michael@axlabs.com>
2020-04-21 18:11:58 +0200

    (minor) Added some documentation and renamed lootAccounts to start with lowercase letter

--- contracts/LootBoxes.sol
+++ contracts/LootBoxes.sol
@@ -8,151 +8,186 @@ contract LootAccount{
         owner = msg.sender;
     }
 
-    //structure to strore the cards hold by an Adress
-    //Every Index of the Array stands for one Card. The Int at the Index for the ammount of that card owned
+    // Structure to store the cards hold by an address
+    // Every index of the array represents the id for one card.
+    // The integer entry represents the amount that is owned.
     struct Loot {
         uint[42] cards;
     }
-    //Structure to store a sell offer
+
+    // Structure to store a sell offer
     struct Offering{
         uint cardNumber;
         uint price ;
         address seller;
     }
-    //Array of all sell offers
+
+    // Array of all sell offers
     Offering[] offerings;
 
     event generatedCard(uint cardNumber, address owner);
     event boughtCard(address owner);
 
-    //mapping of Account to Loot owned
-    mapping (address => Loot) LootAccounts ;
-    //Storage of inducidual Revel Blocknumber
+    // Storage of individual owned loot collection
+    mapping (address => Loot) lootAccounts ;
+
+    // Storage of individual revealBlockNumber
     mapping (address => uint) revealBlockNumber ;
 
+    // Withdraw an amount from the balance of the contract.
     function withdraw(uint amount) public returns (bool) {
+        // Restricted to the owner of this contract
         require(msg.sender == owner, "No Authorization.");
         require(amount <= address(this).balance, "Contract balance too low.");
+
+        // Transfer the amount to the contract owner.
         (bool success, ) = owner.call.value(amount)("");
+
         require(success, "Transfer failed.");
         return true;
     }
 
+    // Check the balance of the contract.
     function getContractBalance() public view returns (uint) {
         require(msg.sender == owner, "No Authorization.");
         return address(this).balance;
     }
 
-    //buy Box by storing revealBlockNumber. Will allow to revel the card after the given Blocknumber
+    // Buy a box by storing revealBlockNumber. This will allow to revel the card after the given block number.
     function buyBox() public payable returns (bool) {
         require(0.1 ether == msg.value);
+
+        // A box can only be bought, if there is no unrevealed box for this buyer.
         require(revealBlockNumber[msg.sender] == 0, "You Still need to claim Loot");
+
+        // Set the revealBlockNumber at 10 blocks after a box has been bought.
         revealBlockNumber[msg.sender] = block.number + 10;
         emit boughtCard(msg.sender);
         return true;
     }
 
-    //return the current revealBlockNumber
+    // Get the current revealBlockNumber.
     function getRevealBlockNumber() public view returns (uint) {
         return revealBlockNumber[msg.sender];
     }
-    // indicate, if the box can already be revealed
+
+    // Indicate if the box can already be revealed.
     function isItReadyYet() public view returns (bool) {
-        if ( revealBlockNumber[msg.sender] == 0 ) {
+        if (revealBlockNumber[msg.sender] == 0) {
             return false;
         }
         return block.number > revealBlockNumber[msg.sender];
     }
 
-    // reveal the Card that has been bought in Box
+    // Reveal the card that has been bought.
     function revealBox() public returns (uint) {
-        //Check that a box has been bouhght and the specified reveal Block has been reached
+        // Check that a box has been bought and the specified revealBlock has been reached.
         require(revealBlockNumber[msg.sender] != 0, "There has no Box been bought");
         require(block.number > revealBlockNumber[msg.sender], "Time has not come yet");
-        //use Blockhash for Number generation
+
+        // Use blockhash for the number generation.
         uint seed = uint(blockhash(revealBlockNumber[msg.sender])) % 1000 + 1;
-        // reset revealBlockNumber so Adress can buy a Box again
+
+        // Reset revealBlockNumber, so that the sender can buy a box again.
         revealBlockNumber[msg.sender] = 0;
-        // determine the card from the Box
+
+        // Determine the card from the box.
         uint card = getCard(seed);
-        //Add card to LootStorage
-        LootAccounts[msg.sender].cards[card]++;
-        //Emitt Card Number and Adress of new card
+
+        // Add the card to LootStorage.
+        lootAccounts[msg.sender].cards[card]++;
+
+        // Emit the card number and address of the newly owned card.
         emit generatedCard(card, msg.sender);
         return card;
     }
 
     function getCard(uint number) private pure returns (uint){
-        //first 22 Card bottom Tier
-        if ( number < 741 ) {
+        // First 22 cards - bottom Tier
+        if (number < 741) {
             return number % 22  ;
-        //next 10 Cards low Tier
+        // Next 10 cards - low Tier
         } else if (number < 951) {
             return number % 10 +22 ;
-        // next 6 Cards mid Tier
+        // Next 6 cards - mid Tier
         } else if (number < 993) {
             return number % 6 + 32 ;
-        // next 2 Cards high Tier
+        // Next 2 cards - high Tier
         } else if (number < 999) {
             return number % 2 + 38 ;
-        // last 2 Cards GOOD Tier
+        // Last 2 cards - GOD Tier
         } else {
             return number % 2 + 40 ;
         }
     }
-    // get all Cards owned by the Adress
+
+    // Get all cards owned by the sender.
     function getCards() public view returns (uint[42] memory) {
-        Loot memory loot = LootAccounts[msg.sender];
+        Loot memory loot = lootAccounts[msg.sender];
         return loot.cards;
     }
-    //create an offering to sell a card. Whenever it is created, the Card is transferd aout of the Loot of the Adress.
+
+    // Create an offering to sell a card.
     function createListing(uint cardNumber, uint myprice ) public returns (bool) {
-        require (LootAccounts[msg.sender].cards[cardNumber] > 0, "You have no Card to sell");
+        require (lootAccounts[msg.sender].cards[cardNumber] > 0, "You have no Card to sell");
         Offering memory myOffering = Offering(cardNumber, myprice, msg.sender);
         offerings.push(myOffering);
-        //transfer Card out off Account
-        LootAccounts[msg.sender].cards[cardNumber]--;
+        
+        // Remove card from account
+        lootAccounts[msg.sender].cards[cardNumber]--;
         return true;
     }
-    //get all the Sell Offers
+
+    // Get all the sell offers
     function getListings() external view returns(Offering[] memory ) {
         return offerings;
     }
-    // Buy a Card that is offerd
-    //in order to be sure to match the right Offering, the index in the Offeringlist and all the data of the Offering is needed to be pased
+
+    // Buy a card that is offered.
+    // In order to be sure to match the right offering, the index in the offering list and all the data of the offering is needed to be passed as parameters.
     function buyCard(uint index ,uint mycardNumber, uint myprice, address myseller) public payable returns (bool) {
-        //check the Offering at the index is the one we want
-        require(offerings[index].cardNumber == mycardNumber,"Order not availible anymore");
-        require(offerings[index].price == myprice,"Order not availible anymore");
-        require(offerings[index].seller == myseller,"Order not availible anymore");
-        require(index < offerings.length,"Order not availible anymore");
-        // check that we have sent the right ammount of money
+        // Check that the offering at the index is the correct one.
+        require(offerings[index].cardNumber == mycardNumber, "Order not availible anymore");
+        require(offerings[index].price == myprice, "Order not availible anymore");
+        require(offerings[index].seller == myseller, "Order not availible anymore");
+        require(index < offerings.length, "Order not availible anymore");
+        
+        // Check that the right amount of eth is set.
         require(msg.value == offerings[index].price,"not enogh Funds provided");
-        // send the Money to the seller
+        
+        // Send the eth to the seller.
         (bool success, ) = offerings[index].seller.call.value(msg.value)("");
         require(success, "Transfer failed.");
-        //remove offering
+        
+        // Remove offering
         removeOffering(index);
-        //add Card to Buyers Loot
-        LootAccounts[msg.sender].cards[mycardNumber]++;
+        
+        // Add Card to buyers loot
+        lootAccounts[msg.sender].cards[mycardNumber]++;
     }
-    //utility to remove an offering from the offerings
+
+    // Removes an offering at a specific index in the list offerings.
     function removeOffering(uint index) internal  {
 
+        // Moves all offerings with a higher index one lower to prevent void entries in the list.
         for (uint i = index; i<offerings.length-1; i++){
             offerings[i] = offerings[i+1];
         }
         offerings.length--;
-
     }
+
+    // Cancels an open offering
     function withdrawOffering(uint index ,uint mycardNumber, uint myprice) public  {
-        //check the Offering at the index is the one we want
-        require(offerings[index].cardNumber == mycardNumber,"Order not availible anymore");
-        require(offerings[index].price == myprice,"Order not availible anymore");
-        require(offerings[index].seller == msg.sender,"Order not availible anymore");
-        //remove offering
+        
+        // Check that the offering at the index is the correct one.
+        require(offerings[index].cardNumber == mycardNumber,"Order not available anymore");
+        require(offerings[index].price == myprice,"Order not available anymore");
+        require(offerings[index].seller == msg.sender,"Order not available anymore");
+        
+        // Remove offering
         removeOffering(index);
-        //add Card back to Sellers Loot
-        LootAccounts[msg.sender].cards[mycardNumber]++;
+
+        // Add card back to sellers loot
+        lootAccounts[msg.sender].cards[mycardNumber]++;
     }
 }
