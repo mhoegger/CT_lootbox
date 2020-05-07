06d4465e1e567aca67a7ee8072aba03197495c1b
mhoegger <32105585+mhoegger@users.noreply.github.com>
2020-05-07 13:37:54 +0200

    added template card

--- src/components/Inventory.vue
+++ src/components/Inventory.vue
@@ -152,7 +152,7 @@ export default {
 }
 .card-grid {
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
   grid-gap: 1rem;
 }
 .hatching-box {
